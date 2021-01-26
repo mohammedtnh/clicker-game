3a05f8a359f432ef574ad6a21e6644d7cc0941fb
Mohamed Hamdan <mohamedhamdan@Mohameds-MacBook-Pro.local>
2021-01-26 17:16:30 +0300

    Player can attack to gain gold

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
+import dragon from "./img/dragon.png";
+import { useState } from "react";
+
 function App() {
+  let [gold, setGold] = useState(0);
+  let [hp, setHp] = useState(5);
+
+  const addGold = (goldGained) => {
+    gold += goldGained;
+    setGold(gold);
+  };
+
+  const hpControl = () => {
+    if (gold < 5) {
+      setHp(5);
+    } else if (gold >= 5 && gold <= 10) {
+      setHp(10);
+    } else if (gold >= 10 && gold <= 20) {
+      setHp(15);
+    } else if (gold >= 20 && gold <= 30) {
+      setHp(20);
+    }
+  };
+
+  const attack = () => {
+    if (hp > 0) {
+      setHp((hp += -1));
+      if (hp === 0) {
+        addGold(1);
+        hpControl();
+      }
+    }
+  };
+
   return (
     <div>
       <br />
-      <p>Gold: 0 </p>
+      <h1>Gold: {gold} </h1>
       <br />
-      <button>Attack</button>
+      <button onClick={attack}>Attack</button>
       <br />
-      <image />
+      <img src={dragon} alt="dragon" />
+      <h1>Hp: {hp} </h1>
     </div>
   );
 }
